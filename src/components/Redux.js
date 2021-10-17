6a3bafd6541728de23b2882d3d63fc7b53f5de32
NickBwalley <nickbwalley@gmail.com>
2021-10-17 08:10:32 +0300

    react-redux

---
+++ src/components/Redux.js
@@ -0,0 +1,87 @@
+// console.clear()
+
+const createPolicy = (name, amount) => {
+  return {
+    type: "CREATE_POLICY",
+    payload: {
+      name: name,
+      amount: amount,
+    },
+  };
+};
+
+const deletePolicy = (name) => {
+  return {
+    type: "DELETE_POLICY",
+    payload: {
+      name: name,
+    },
+  };
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: "CREATE_CLAIM",
+    payload: {
+      name: name,
+      amountOfMoneyToCollect: amountOfMoneyToCollect,
+    },
+  };
+};
+
+//Reducers (departments)
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type === "CREATE_ACTION") {
+    // we care about this action
+    //oldListOfClaims.push(action.payload) => Alternative Diference is that we are modifying an existing array but this method below we create a brand new array. Check example below.
+    return [...oldListOfClaims, action.payload]; //ES2015 syntax
+  }
+
+  // we don't care about this action.(form!!)
+  return oldListOfClaims;
+  // Example ES2015 syntax.
+  /*
+  const numbers = [1,2,3]
+  [...numbers, 4]  // create a new array and append the number 4 at the end.
+  returns [1,2,3,4]
+  */
+};
+
+const Accounting = (bagOfMoney = 100, action) => {
+  if (action.type === "CREATE_CLAIM") {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === "CREATE_POLICY") {
+    return bagOfMoney + action.payload.amount;
+  }
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === "CREATE_POLICY") {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === "DELETE_POLICY") {
+    return listOfPolicies.filter((name) => name !== action.payload.name);
+  }
+  return listOfPolicies;
+};
+
+const { createStore, combineReducers } = Redux;
+
+const ourDepartments = combineReducers({
+  accounting: accounting,
+  claimsHistory: claimsHistory,
+  policies: policies,
+});
+
+const store = createStore(ourDepartments);
+
+store.dispatch(createPolicy("Alex", 20));
+store.dispatch(createPolicy("Jim", 30));
+store.dispatch(createPolicy("Bob", 40));
+
+store.dispatch(createClaim("Alex", 120));
+store.dispatch(createClaim("Jim", 50));
+
+store.dispatch(deletePolicy("Bob"));
+
+console.log(store.getState());
